60a6a2f7d4c1e071e275094fc87e111e46b28ed2
marcelounb <marcelounb@gmail.com>
2022-02-02 09:27:29 -0300

    var e conversões

---
+++ 2-variaveis.js
@@ -0,0 +1,30 @@
+let nome, sobrenome
+let nomeCompleto
+
+nome = "Teresinha"
+sobrenome = "Telesena"
+var telefone
+telefone = 4271994
+
+
+if(nome == "Teresinha"){
+    let nomeCompleto = nome + sobrenome  //Colocando let aqui ela fica local apenas, sem acesso externo
+    console.log(nomeCompleto)
+    let telefone
+    telefone = 3333333
+    console.log(telefone)
+}
+
+console.log(nomeCompleto)
+console.log(telefone)
+
+var var1 = 123
+var var2 = "123"
+var var3 = var1+var2
+
+console.log(var1 === var2)
+console.log(var1 == var2)
+console.log(var3)
+console.log(typeof var3)
+console.log(var1 === parseInt(var2))
+
